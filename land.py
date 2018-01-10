84372dfe62f18191b91af0e3ec2ce5e68ceed251
ljbelenky <ljbelenky@gmail.com>
2018-01-10 13:59:47 -0700

    date intervals and capitalization

--- land.py
+++ land.py
@@ -1,8 +1,72 @@
 import json
 import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from nltk.stem.snowball import SnowballStemmer
+from string import punctuation
+from nltk import word_tokenize
+
+
+class TextClassifier(object):
+    """A text classifier model:
+        - Vectorize the raw text into features.
+        - Fit a naive bayes model to the resulting features.
+    """
+
+    def __init__(self):
+        self._vectorizer = TfidfVectorizer(tokenizer = self.tokenizer, stop_words = 'english')
+        self._classifier = MultinomialNB()
+
+    def tokenizer(self,text):
+        sbs = SnowballStemmer('english')
+        punct = set(punctuation)
+        return [sbs.stem(token) for token in word_tokenize(text) if token not in punct]
+
+    def fit(self, X, y):
+        """Fit a text classifier model.
+
+        Parameters
+        ----------
+        X: A numpy array or list of text fragments, to be used as predictors.
+        y: A numpy array or python list of labels, to be used as responses.
+
+        Returns
+        -------
+        self: The fit model object.
+        """
+        # Code to fit the model.
+        X = self._vectorizer.fit_transform(X)
+        self.mnb = MultinomialNB()
+        self.mnb.fit(X,y)
+        return self.mnb
+
+    def predict_proba(self, X):
+        """Make probability predictions on new data."""
+        X = self._vectorizer.transform(X)
+        return self.mnb.predict_proba(X)
+
+    def predict(self, X):
+        """Make predictions on new data."""
+        X = self._vectorizer.transform(X)
+        return self.mnb.predict(X)
+
+    def score(self, X, y):
+        """Return a classification accuracy score on new data."""
+        X = self._vectorizer.transform(X)
+        return self.mnb.score(X,y)
+
+
+def percent_capitals(text):
+    text = [a for a in text if a in "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"]
+
+    original_length = len(text)
+    if original_length == 0: return 0
+    capitals = len([x for x in text if x!=x.lower()])
+    return capitals/original_length
+
 
 df = pd.read_json('data/data.json')
-df = df.iloc[:200,:]
+#f = df.iloc[:200,:]
 
 df['target'] = df.acct_type.apply(lambda x:1*(x[:5]=='fraud'))
 df['listed'] = df.listed.apply(lambda x:1*(x=='y'))
@@ -22,22 +86,29 @@ categoricals = ['channels', 'org_facebook', 'org_twitter', 'fb_published', 'deli
 for cat in categoricals:
     df[cat] = df[cat].apply(lambda x:str(x))
 
+'''CALCULATE PERCENT CAPITALS'''
+df['description_capitals'] = df.description.apply(lambda x:percent_capitals(x))
+df['name_capitals'] = df.name.apply(lambda x:percent_capitals(x))
+
 
 ''' CALCULATE TIME INTERVALS'''
-df['start_to_pay'] = df['approx_payout_date'] - df['event_start']
-df['create_to_pay'] = df['approx_payout_date'] - df['event_created']
-df['duration'] = df['event_end'] - df['event_start']
-df['user_to_create_event'] = df['event_created']-df['user_created']
-df['user_to_publish'] = df['event_published'] - df['user_created']
-df['publish_to_start'] = df['event_start'] - df['event_published']
+s_d = 3600*24
+df['start_to_pay'] = (df['approx_payout_date'] - df['event_start'])/s_d
+df['create_to_pay'] = (df['approx_payout_date'] - df['event_created'])/s_d
+df['duration'] = (df['event_end'] - df['event_start'])/s_d
+df['user_to_create_event'] = (df['event_created']-df['user_created'])/s_d
+df['user_to_publish'] = (df['event_published'] - df['user_created'])/s_d
+df['publish_to_start'] = (df['event_start'] - df['event_published'])/s_d
 
-# df = df.drop(['approx_payout_date','event_start','approx_payout_date','event_created','event_end', 'event_published', 'user_created'], axis = 1)
+df = df.drop(['approx_payout_date','event_start','approx_payout_date','event_created','event_end', 'event_published', 'user_created'], axis = 1)
 
 '''TFIDF ON TEXT COLUMNS'''
 tf_idf_columns = ['description']
 df['description_prob'] = 0
 
-dummy_columns = [col for col in df.columns if df[col].dtype=='O'].remove('description')
+dummy_columns = [col for col in df.columns if df[col].dtype=='O']
+
+dummy_columns.remove('description')
 
 
 for col in dummy_columns:
@@ -49,3 +120,7 @@ for col in dummy_columns:
         dummies = pd.get_dummies(df[col], prefix = col, dummy_na = True, drop_first = True)
         df = pd.concat([df, dummies], axis = 1)
     del df[col]
+
+text_c = TextClassifier()
+text_c.fit(df.description, df.target)
+df['description_prob'] = text_c.predict_proba(df.description)
